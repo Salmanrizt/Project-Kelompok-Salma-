809767d79589e6cfe9aec3d3afe4d7c9fc004ac9
zazeza <zizypizy31@gmail.com>
2025-06-03 16:48:55 +0700

    Update server.hpp

--- server.hpp
+++ server.hpp
@@ -1,1 +1,10 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
 
+#include <string>
+
+bool checkStudentInDatabase(const std::string& id);
+void updateAttendance(const std::string& id);
+std::string getCurrentTimestamp();
+
+#endif
