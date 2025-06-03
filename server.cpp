5852a1a71b90526f2646f5c3c65109a0397d6849
Salmanrizt <salmanadirariztisa@gmail.com>
2025-06-03 22:17:31 +0700

    main function

--- server.cpp
+++ server.cpp
@@ -92,3 +92,66 @@ void updateAttendance(const std::string& id) {
     }
     outfile2.close();
 }
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+
+    // buat socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // set reuse address
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // bind socket ke port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // listen koneksi masuk
+    if (listen(server_fd, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Server running on port " << PORT << std::endl;
+
+    while (true) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        memset(buffer, 0, BUFFER_SIZE);
+        ssize_t valread = read(new_socket, buffer, BUFFER_SIZE);
+        if (valread > 0) {
+            std::string id(buffer);
+
+            if (checkStudentInDatabase(id)) {
+                updateAttendance(id);
+                std::string msg = "Attendance recorded for ID: " + id + "\n";
+                send(new_socket, msg.c_str(), msg.size(), 0);
+            } else {
+                std::string msg = "Error: Student ID not found\n";
+                send(new_socket, msg.c_str(), msg.size(), 0);
+            }
+        }
+        close(new_socket);
+    }
+
+    return 0;
+}
